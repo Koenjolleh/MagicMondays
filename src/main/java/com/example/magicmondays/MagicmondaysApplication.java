f40a15c1244fc06be21b38c2b84c065c48e7c1d8
Jakob Olsen <Koen_jolle@hotmail.com>
2020-08-05 23:29:04 +0200

    Initial commit

---
+++ src/main/java/com/example/magicmondays/MagicmondaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.magicmondays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicmondaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicmondaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/magicmondays/MagicmondaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magicmondays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicmondaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
