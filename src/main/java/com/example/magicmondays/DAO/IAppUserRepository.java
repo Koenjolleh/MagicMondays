a916f3f290ed448700ce56e29824c2d296a4ce96
Jakob Olsen <Koen_jolle@hotmail.com>
2021-03-25 01:04:46 +0100

    Updated hibernate and ERD

--- src/main/java/com/example/magicmondays/DAO/IAppUserRepository.java
+++ src/main/java/com/example/magicmondays/DAO/IAppUserRepository.java
@@ -1,9 +1,9 @@
 package com.example.magicmondays.DAO;
 
-import com.example.magicmondays.Model.Player;
+import com.example.magicmondays.Model.Gandalf;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IPlayerRepository extends JpaRepository<Player, Long> {
+public interface IGandalfRepository extends JpaRepository<Gandalf, Long> {
 }
